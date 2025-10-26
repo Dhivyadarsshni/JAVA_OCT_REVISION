645985d7e23a2e9fc9dde58f058418b8d2d5ad94
Dhivyadarsshni <dhivyadarsshni16@gmail.com>
2025-10-26 13:24:20 +0530

    basic sum problem

--- src/main/java/BasicSyntax.java
+++ src/main/java/BasicSyntax.java
@@ -1,2 +1,22 @@
-package PACKAGE_NAME;public class BasicSyntax {
+import java.util.Scanner;
+
+//Q)Print the sum of two numbers entered by the user.
+
+public class BasicSyntax {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int n1,n2,sum;
+
+        System.out.print("Enter number 1: ");
+        n1 = sc.nextInt();
+
+        System.out.print("Enter number 2: ");
+        n2 = sc.nextInt();
+
+        sum = n1+n2;
+
+        System.out.println("Sum : "+sum);
+    }
 }
